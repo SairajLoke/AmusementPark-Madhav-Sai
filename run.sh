5df4d6cd87d7c7959f12bebd106ce34f12204373
Madhav Kadam <mkiitind@gmail.com>
2024-04-11 19:26:52 +0530

    just synced progress

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-g++ bmpLoader.cpp ./src/shapes/shapes.cpp ./src/background/background.cpp  ./src/human/human.cpp ./src/objects/objects.cpp ./src/rides/rides.cpp ./src/light/light.cpp main.cpp -lglut -lGLU -lGL && ./a.out
-
+cd build/
+make
+./cs352
+cd ..
